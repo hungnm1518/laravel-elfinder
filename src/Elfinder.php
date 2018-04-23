c45ae3cb7287fa80895b7ec7c7e12e1444822e5f
Hưng Nguyễn <1074467+hungnm144@users.noreply.github.com>
2018-04-23 16:03:57 +0700

    Update Elfinder.php

--- src/Elfinder.php
+++ src/Elfinder.php
@@ -1,6 +1,8 @@
-<?php namespace Barryvdh\Elfinder;
+<?php
 
-class Elfinder{
+namespace Hungnm\Elfinder;
+
+class Elfinder {
     public static function checkAccess($attr, $path, $data, $volume) {
         return strpos(basename($path), '.') === 0       // if file/folder begins with '.' (dot)
             ? !($attr == 'read' || $attr == 'write')    // set read+write to false, other (locked+hidden) set to true
